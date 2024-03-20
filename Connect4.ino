3270953099669a99907fd68f1c02bd65f4c6193f
Rio <111706275+YargleWithWings@users.noreply.github.com>
2024-03-20 01:58:18 -0400

    Deleted and partially remade checkWin function

--- Connect4.ino
+++ Connect4.ino
@@ -1,11 +1,11 @@
-#include <FastLED.h>     // include the FastLED library code
+#include <FastLED.h> // include the FastLED library code
 
 // define constants
-#define DATA_PIN     2
-#define NUM_LEDS    50
+#define DATA_PIN 2
+#define NUM_LEDS 50
 #define MAX_BRIGHTNESS 255
-#define BRIGHTNESS  35   // MAX is 255
-#define LED_TYPE    WS2811
+#define BRIGHTNESS 35 // MAX is 255
+#define LED_TYPE WS2811
 #define ROWS 7
 #define COLS 7
 
@@ -13,19 +13,14 @@
 CRGB leds[NUM_LEDS];
 
 // the light arrangement
-byte cols[ROWS][COLS+1] = {{7,48,47,46,45,44,43,42},{7,35,36,37,38,39,40,41},{7,34,33,32,31,30,29,28},{7,21,22,23,24,25,26,27},{7,20,19,18,17,16,15,14},{7,7,8,9,10,11,12,13},{7,6,5,4,3,2,1,0}};
+byte cols[ROWS][COLS + 1] = {{7, 48, 47, 46, 45, 44, 43, 42}, {7, 35, 36, 37, 38, 39, 40, 41}, {7, 34, 33, 32, 31, 30, 29, 28}, {7, 21, 22, 23, 24, 25, 26, 27}, {7, 20, 19, 18, 17, 16, 15, 14}, {7, 7, 8, 9, 10, 11, 12, 13}, {7, 6, 5, 4, 3, 2, 1, 0}};
 
-//the current state of the board
+// the current state of the board
 byte board[ROWS][COLS];
-for (int i = 0; i < ROWS; ++i) {
-  for (int j = 0; j < COLS; ++j) {
-    board[i][j] = 2;
-  }
-}
 
-bool playerTurn = 1; // 1 is red, 0 is blue
-byte columnButtonPins[COLS] = {3, 4, 5, 6, 7, 8, 9};  // Pins connected to buttons to drop pieces
-byte resetButtonPin = 10; // Button to reset the board
+bool playerTurn = 1;                                 // 1 is red, 0 is blue
+byte columnButtonPins[COLS] = {3, 4, 5, 6, 7, 8, 9}; // Pins connected to buttons to drop pieces
+byte resetButtonPin = 10;                            // Button to reset the board
 bool gameOver = false;
 
 // Runs once at the start of the program
@@ -33,6 +28,7 @@ void setup() {
   Serial.begin(9600);
   FastLED.addLeds<LED_TYPE, DATA_PIN, RGB>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
   FastLED.setBrightness(BRIGHTNESS);
+  resetBoard();
   for (int i = 0; i < COLS; i++) {
     pinMode(columnButtonPins[i], INPUT_PULLUP);
   }
@@ -60,93 +56,107 @@ void loop() {
   delay(50);
 }
 
-void reset() //resets the board
-{
-  for(byte x = 0; x < COLS; x++){
-      cols[x][0] = 7;
-      leds_init();
-    }
-    leds[49] = CRGB::Red;
-    playerTurn = 1;
-    gameOver = false;
-    FastLED.show();
+void reset() { // resets the board
+  for (byte x = 0; x < COLS; x++) {
+    cols[x][0] = 7;
+  }
+  resetBoard();
+  leds_init();
+  leds[49] = CRGB::Red;
+  playerTurn = 1;
+  gameOver = false;
+  FastLED.show();
 }
 
-void leds_init() //turns off all LEDs
-{
-  for ( byte x = 0; x < NUM_LEDS; x=x+1 ) {
+void resetBoard() {
+  for (int i = 0; i < ROWS; ++i) {
+    for (int j = 0; j < COLS; ++j)
+    {
+      board[i][j] = 2;
+    }
+  }
+}
+
+void leds_init() { // turns off all LEDs
+  for (byte x = 0; x < NUM_LEDS; x++) {
     leds[x] = CRGB::Black;
   }
 }
 
+void drop(byte column) {
+  // WHY IS IT COLUMN ROW NOT ROW COLUMN??
+  if (cols[column][0] > 0 && !gameOver) { // If the column is not full, drop
+    board[cols[column][0]][column] = playerTurn; // Set the board state
 
-void drop(byte column){
-  if(cols[column][0] > 0 && !gameOver) {
-  board[cols[column][0]][column] = playerTurn;
-  checkWin(column, cols[column][0]);
-  CRGB color = CRGB::Blue;
-  if (playerTurn){
-    color = CRGB::Red;
-    playerTurn = 0;
-  }
-  else {
-    playerTurn = 1;
-  }
-  if (!playerTurn) {
-    leds[49] = CRGB::Blue;
-  }
-  else {
-    leds[49] = CRGB::Red;
-  }
+    // Set the color and the turn indicator
+    CRGB color = CRGB::Blue;
+    if (playerTurn) {
+      color = CRGB::Red;
+      playerTurn = 0;
+      leds[49] = CRGB::Blue;
+    }
+    else {
+      playerTurn = 1;
+      leds[49] = CRGB::Red;
+    }
 
-  for (byte x = 1; x < cols[column][0];  x=x+1 ){
-    leds[cols[column][x]] = color;
-    FastLED.delay(100);
-    leds[cols[column][x]] = CRGB::Black;
-    FastLED.delay(10);
-  }
-  
-  leds[cols[column][cols[column][0]]] = color;
+    // display the piece dropping
+    for (byte x = 1; x < cols[column][0]; x++) {
+      leds[cols[column][x]] = color;
+      FastLED.delay(100);
+      leds[cols[column][x]] = CRGB::Black;
+      FastLED.delay(10);
+    }
 
-  FastLED.delay(1);
-  cols[column][0] -= 1;
+    // display the piece at the bottom
+    leds[cols[column][cols[column][0]]] = color;
+
+    FastLED.delay(1);
+
+    //check if the player won
+    checkWin(column);
+
+    // update the row we're on
+    cols[column][0] -= 1;
   }
 }
 
-void checkWin(byte playedCol, byte playedRow) {
-  //declare bounds
+void checkWin(byte playedCol) {
+  byte playedRow = cols[playedCol][0];
+  bool foundWin = false;
+  // declare bounds
   byte leftmostCol = max(playedCol - 3, 0);
   byte rightmostCol = min(playedCol + 3, COLS - 1);
   byte bottomRow = max(playedRow - 3, 0);
   byte topRow = min(playedRow + 3, ROWS - 1);
 
-  byte numFoundInARow = 0;
-  
-  //checking Horizontal
-  byte score = 1;
-  byte count = 1;
-   
-   while((x+count >= 0) && (x+count < WIDTH))
-   {
-      if (board_info[y][x+count] == player)  //Check Left
-      {
-         score++;
-         count++;
+  // https://stackoverflow.com/questions/39062111/java-how-to-check-diagonal-connect-four-win-in-2d-array
+  // check vertical
+  byte numFoundInARow = 1;
+  if (cols[column][0] > 3) {
+    for (byte i = 1; i < 4; i++) {
+      if (board[playedRow][playedCol] == board[playedRow][playedCol - i]) {
+        numFoundInARow++;
       }
-      else break;  //If no combo is detected break from the loop
-   }
+      else {
+        break;
+      }
+    }
+  }
+  if (numFoundInARow == 4) {
+    foundWin = true;
+  }
 
-   count = 1;
-   while((x-count < WIDTH) && (x-count >=0))
-   {
-      if (board_info[y][x-count] == player)  //Check Right
-      {
-         score++;
-         count++;
-      }   
-      else break;  //If no combo is detected break from the loop
-   }
-   
-   if (score == 4) return true;
+  // check horizontal
+  numFoundInARow = 1;
+
+
+
+
+
+  // check vertical
+
+
+  if (score == 4)
+    return true;
 }
-
